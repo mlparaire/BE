3f2a53508e96e8d57aa5d5aec05ba44d42a7555f
Martin <martin.paraire@egis-group.com>
2025-09-25 15:32:15 +0200

    Trying to resolve merging

--- Encrypt.py
+++ Encrypt.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List,Tuple
 
 # type Vector = List[float]
 #type Address = tuple[str,int]
@@ -10,13 +10,12 @@ class Encrypt:
     ## and thus returns a List of integers
     def hexify(self,characters : str) -> List[int]:
         return [ord(char) for char in characters]
-
     ## We initiate the class
-    def __init__(self,message : List[str]):
-        self.__K = {'clear' : "blockchains",'hex' : self.hexify('bitcoin')} ## This the key, with clear and hex form. Note that we load it as a private variable, so it cannot be accessed outside this class
-        self.message = {'clear': message} ## This is where we load the messages. We load it as List[str] so we can store them in the same dictionnary
+    def __init__(self,message : str):
+        self.__K = {'clear' : "blockchains",'hex' : self.hexify('blockchains')} ## This the key, with clear and hex form. Note that we load it as a private variable, so it cannot be accessed outside this class
+        self.message = {'clear': message,'hex':[self.hexify(hex) for hex in message]} ## This is where we load the messages. We load it as List[str] so we can store them in the same dictionnary
 
     def characterize(self,hexes:List[int]) -> str:
         return ''.join([chr(hex) for hex in hexes])
-
-# For Commit
+    def XOR_bitwise_encrypting(self,message : List[int]) -> List[int]:
+        return [a^b for a,b in zip(message,self.__K['hex'] if len(message) == len(self.__K['hex']) else (lambda mod: self.__K['hex'] * mod[0] + self.__K['hex'] * mod[1])(divmod(len(message), len(self.__K["hex"]))))]
