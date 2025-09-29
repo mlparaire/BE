ee3cad4dff703ff516c87579d4cbf501b13f0855
Martin <martin.paraire@egis-group.com>
2025-09-29 21:09:04 +0200

    Final

--- Encrypt.py
+++ Encrypt.py
@@ -1,21 +1,105 @@
-from typing import List,Tuple
+# We import all the packages we need
+from typing import List
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa as RSA
+from cryptography.exceptions import InvalidSignature
+from hashlib import sha256
+from time import time
+import os
+import multiprocessing as mp
+from concurrent.futures import ProcessPoolExecutor
+from binascii import hexlify ,unhexlify
 
-# type Vector = List[float]
-#type Address = tuple[str,int]
+# We define a custom type vector for our functions
+type vector = List[int]
 
-## This class will store all the values we need for this exercice and the functions we need
+## This class will store all the values and function we need for the first part of  exercice 1
 class Encrypt:
-    ## We define the function hexify first, as we will use it on the key
+    ## We define the function hexify first, as we will use it on the key and the messages
     ## This function returns the unicode for every character of the string entered into a function
     ## and thus returns a List of integers
-    def hexify(self,characters : str) -> List[int]:
+    def unicode_encoding(self,characters : str) -> vector:
         return [ord(char) for char in characters]
-    ## We initiate the class
-    def __init__(self,message : str):
-        self.__K = {'clear' : "blockchains",'hex' : self.hexify('blockchains')} ## This the key, with clear and hex form. Note that we load it as a private variable, so it cannot be accessed outside this class
-        self.message = {'clear': message,'hex':[self.hexify(hex) for hex in message]} ## This is where we load the messages. We load it as List[str] so we can store them in the same dictionnary
 
-    def characterize(self,hexes:List[int]) -> str:
+    ## We initiate the class with the key and the message
+    def __init__(self,message : List[str]): # Note that we define message as str, not List[str]. But we can still load the two message as the same time
+        self.__K = {'clear' : "blockchain",'hex' : self.unicode_encoding('blockchain')} ## This the key, with clear and hex form. Note that we load it as a private variable, so it cannot be accessed outside this class
+        self.message = {'clear': message,'hex':[self.unicode_encoding(hex) for hex in message]} ## This is where we load the messages. We load it as List[str] so we can store them in the same dictionnary, and we transform them into hexbits format
+
+    # we create a function to return go from unicode encoding to characters. We take each byte from the message and concatenate each characterized digit into a single string
+    def characterize(self,hexes:vector) -> str:
         return ''.join([chr(hex) for hex in hexes])
-    def XOR_bitwise_encrypting(self,message : List[int]) -> List[int]:
-        return [a^b for a,b in zip(message,self.__K['hex'] if len(message) == len(self.__K['hex']) else (lambda mod: self.__K['hex'] * mod[0] + self.__K['hex'] * mod[1])(divmod(len(message), len(self.__K["hex"]))))]
+
+    ## With this function, we perform the bitwise XOR.
+    ## To make sure the key is always the same length as the message, we check if length are equal between the two
+    ## If not, we compute the modulo division of both length. We then multiply the password by the quotient, and add the number of characters equals to the remainder
+    ## The function returns the encrypted message in hexadecimal string
+    def XOR_bitwise_encrypting(self,message : vector) -> str:
+        return hexlify(bytes([a^b for a,b in zip(message,self.__K['hex'] if len(message) == len(self.__K['hex']) else (lambda mod: self.__K['hex'] * mod[0] + self.__K['hex'] * mod[1])(divmod(len(message), len(self.__K["hex"]))))])).decode('utf8')
+    ## With this function, we reverse the bitwise XOR from the previous function
+    ## Using the same length equalization technique for the key as the previous function
+    ## We simply apply the bitwise XOR to the encrypted message, with the same key
+    ## The function returns tbe decrypted message in plaintext
+    def XOR_decode(self,encoded_string: str) -> str:
+        return self.characterize([a ^ b for a, b in zip(list(unhexlify(encoded_string)), self.__K['hex'] if len(encoded_string) == len(self.__K['hex']) else (lambda mod: self.__K['hex'] * mod[0] + self.__K['hex'] * mod[1])(divmod(len(encoded_string), len(self.__K["hex"]))))])
+
+## For the second part of first exercice, we create a new class. Because we won't need information from the first class, we don't need to make an inheritance class
+## We call it PKE for Private-Public Key Encryption
+class PKE:
+    def __init__(self,message:str):
+        self.__public_key_b = serialization.load_pem_public_key(bytes(open('public_key.pem').read().encode('utf8'))) # Here, we load the public key from Louis Bertucci's website. Note that we load it as a private variable
+        if not {'private_key_paraire_23300561.pem', 'public_key_paraire_23300561.pem'}.issubset(os.listdir()): # Here, we check that we haven't created keys pairs for this exercice already
+            self.__private_key = RSA.generate_private_key(public_exponent=65537,key_size=2048) # If not already create, we creat the private key. We also load it as a private variable
+            self.public_key = self.__private_key.public_key() # We derive the public key from the private key
+            open('private_key_paraire_23300561.pem','wb').write(self.__private_key.private_bytes(encoding=serialization.Encoding.PEM,format = serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm=serialization.NoEncryption())) # To save the newly created keys, we save them both inside distincts pem files
+            open('public_key_paraire_23300561.pem','wb').write(self.public_key.public_bytes(encoding=serialization.Encoding.PEM,format = serialization.PublicFormat.SubjectPublicKeyInfo))
+        else: # Here, if we have already created they keys, we simply load them.
+            self.__private_key = serialization.load_pem_private_key(open('private_key_paraire_23300561.pem','rb').read(),password=None)
+            self.public_key = serialization.load_pem_public_key(bytes(open('public_key_paraire_23300561.pem').read().encode('utf8')))
+        self.message = message.encode()
+    ## With the function RSA_public_key_encryption, we encrypt message as a string in hexadecimal form.
+    ## To do so, we use Louis Bertucci's public key to encrypt our message. Only Louis Bertucci's private key will be able to decipher the message
+    ## The function returns a string in hexadecimal form
+    def RSA_public_key_encryption(self,message : bytes) -> str:
+        return hexlify(self.__public_key_b.encrypt(message,padding=padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))).decode("utf8")
+    ## With the function sign_message, we sign a message in plaintext using our private key
+    ## The function returns both the message in hexadecimal form and the signature in a tuple
+    def sign_message(self) -> tuple:
+        return hexlify(self.message).decode(), hexlify(self.__private_key.sign(self.message,padding=padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),algorithm=hashes.SHA256())).decode()
+    ## With the function check_signature, we check if the signature is correct with our public key
+    ## To do so, we implement a try-except loop. If the public key verify our signature, the function return a message of succes
+    ## However, if the public key raise a InvalidSignature exception, the function raise a ValueError
+    def check_signature(self,message:str,signature :str):
+        try:
+            self.public_key.verify(unhexlify(signature),unhexlify(message),padding=padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),algorithm=hashes.SHA256())
+            return "The message has been successfully verified with the signature"
+        except InvalidSignature:
+            raise ValueError(f'The signature could not be verified: Invalid signature')
+
+## For the second question, we create a third class called Hash_func, that will be used to compute proof-of-work hashes
+## Because we don't need any informations from the previous questions, we don't add an inheritance architecture
+class Hash_func():
+    def __init__(self):
+        self.algoritm = sha256 ## Here, as the hashing function, we load sha256 from hashlib
+        self.message = 'Paraire:"DigitalEconomics":' ## Here, we load the first part of the message
+
+    ## With the function compute_hash, we seek to imitate a proof-of-work resolution
+    ## We call the function with the complexity variable, that defines the number of liminary zeros in the hash
+    ## The function returns a tuple with the message and the number of nonce, the hash and the time taken for the resolution
+    def compute_hash(self,complexity : int) -> tuple:
+         ## Here, we define the prefix
+        nonce = 0 ## Here, we define the number of nonce
+        start_time = time() ## To compute the total time spent on the problem, we save the starting time
+        # To check every hash until we find one that fits our needs, we use a while loop that stops whence a hash function with the correct liminary zeros is found.
+        # nonce is incremented each time we fail to find a correct hash
+        while not(self.algoritm((self.message+str(nonce)).encode()).hexdigest().startswith(str('0' * complexity))):
+            # Note that we use the walrus operator
+            print(self.algoritm((self.message+str(nonce)).encode()).hexdigest()) if self.algoritm((self.message+str(nonce)).encode()).hexdigest().startswith('0') else ''
+            nonce += 1
+        end_time = time() ## We note the end time
+        return self.message+str(nonce),sha256((self.message + str(nonce)).encode()).hexdigest(),nonce,"Execution time ~ {0:.2f} seconds ".format(end_time-start_time)
+
+    def parralelized_work(self,complexity:int) -> tuple:
+        pass
--- PS_1.py
+++ PS_1.py
@@ -1,14 +1,67 @@
-from Encrypt import Encrypt
-from operator import xor
-#Question 1
+from Encrypt import Encrypt,PKE,Hash_func
+
+#Exercice 1
+
+# Question 1
+
+## For this question, all of the bitwise operations will be computed within the class Encrypt.
+## This way, we can use private variables so to safely contains the private key
 
 ## We load the class Encrypt from the file Encrypt.py, and we initiate with the messages from the exercice
-enc = Encrypt(["bitcoin",'This is such a great class'])
+enc = Encrypt(["bitcoin","This is such a great class"])
 
-## The key has already been transformed into digits, and we need to do the same with the function hexify
-## to the messages from the exercice
+# We load the first message from the class, and apply a bitwise XOR
+first_message = enc.XOR_bitwise_encrypting(enc.message['hex'][0])
+print(f'The first message, when encoded in hexadecimal form is : {first_message}')
+# We load the second message from the class, and apply a bitwise XOR
+second_message = enc.XOR_bitwise_encrypting(enc.message['hex'][1])
+print(f'The second message, where encoded in hexadecimal form is: {second_message}')
 
-#print(enc.message['hex'][1])
-#print(enc.XOR_bitwise_encrypting(enc.message['hex']))
+# To check whether we can decipher our encrypted message, we perform a new bitwise XOR with the same key to the cyphertext
+print(f'The first message, when decoded is: {enc.XOR_decode(first_message)}')
+print(f'The first message, when decoded is: {enc.XOR_decode(second_message)}')
 
-# FOr commit
+
+# Question 2
+# We load the class PKE, that contains all public and private keys, with a message that will be used for part 3
+pke = PKE("The message is fragments.lameness.clowns")
+
+## Part 1
+# We use the class function RSA_public_key_encryption to encrypt a message
+message_for_b = pke.RSA_public_key_encryption(b"Dear Professor Bertucci\n, here's my message: Les sanglots longs des violons de l'automne \n blessent mon coeur d'un langueur monotone")
+print(f'The message, in hexadecimal form is:\n{message_for_b}')
+
+## Part 2
+# The pair of keys is generated when the function PKE is loaded
+
+## Part 3
+# We extract the message from initialisation with its signature from the private key in two variables
+message, signature = pke.sign_message()
+print(f'The message is {message}\nThe signature is {signature}')
+print(f'We now verify the signature')
+# We verify the signature with our public key inside the class PKE
+try:
+    # If the signature checks out, we print a success message
+    print(f"\033[92m{pke.check_signature(message=message,signature=signature)}\033[0m")
+except ValueError as e:
+    # If not, we print an error message
+    print(f"\033[91m{e}\033[0m")
+
+## Part 4
+# Using the message in the initialisation of PKE class, we encrypt it using the encryption function with Louis Bertucci's class
+message = pke.RSA_public_key_encryption(pke.message)
+print(message)
+
+# Question 2
+print('Question 2 \n')
+# We load the class Hash_func, that contains the hashing SHA256 algorithm
+test = Hash_func()
+# We attempt to compute hashes that meets 5 and 10 liminary zeros, and save the result
+response,second_response= test.compute_hash(5),test.compute_hash(6)
+with open('Exercice_2_answer_Paraire_22300561.txt','wb') as ex:
+    ex.write(b'For the computation of a proof of work with 5 leading zeros, the results are :\n')
+    ex.write((("Message M = "+response[0]+'\nHash = '+response[1]+'\nNumber of nonce = '+str(response[2])+"\n"+response[3]).encode()))
+    ex.write(b'\n\n')
+    ex.write(b'For the computation of a proof of work with 10 leading zeros, the results are :\n')
+    ex.write((("Message M = " + second_response[0] + '\nHash = ' + second_response[1] + '\nNumber of nonce = ' + str(second_response[2]) + "\n" + second_response[3]).encode()))
+    ex.close()
